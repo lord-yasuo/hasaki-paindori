5a29022644b46fccf0594bce9d1324a63320b0d2
yasuo.lord <hasaki.yasuo@protonmail.com>
2022-02-03 22:33:21 +0100

    Change deploy script

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,4 @@ cd dist
 git init
 git add .
 git commit -m 'New Deployment'
-git push -f https://github.com/lord-yasuo/hasaki-paindori.git main:prod
+git push -f https://github.com/lord-yasuo/hasaki-paindori.git master:prod
