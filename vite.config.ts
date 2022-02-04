18bb2e62ec247a9697e6aed87f175afded35fb4d
yasuo.lord <yasuo.lord@protonmail.com>
2022-02-04 01:39:09 +0100

    Clean packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [
-    vue(),
-    createHtmlPlugin({
-      minify: true,
-      entry: 'src/main.ts',
-      template: 'index.html',
-      inject: {
-        data: {
-          title: 'Hasaki Paindori',
-          injectScript: `A fansub webpp`,
-        },
-      },
-    })
+    vue()
   ],
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      '@': path.resolve(__dirname, 'src'),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     }
   }
 });
