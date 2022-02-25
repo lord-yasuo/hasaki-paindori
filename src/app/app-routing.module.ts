8d319bf1ff9cd49a8938a8cf586bebe5adccc213
yasuo.lord <yasuo.lord@protonmail.com>
2022-02-25 17:40:28 +0100

    Add route animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,53 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    data: { label: 'Accueil' }
   },
   {
     path: 'anime',
-    redirectTo: 'catalogue'
+    redirectTo: 'catalogue',
+    data: { label: 'Catalogue' }
   },
   {
     path: 'anime/:id',
-    loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule)
+    loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule),
+    data: { label: 'Anime' }
   },
   {
     path: 'anime/:id/:name',
-    loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule)
+    loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule),
+    data: { label: 'Anime' }
   },
-  { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
   {
-    path: 'agenda', loadChildren: () => import('./agenda/agenda.module').then(m => m.AgendaModule)
+    path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule),
+    data: { label: 'Not found' }
+  },
+  {
+    path: 'agenda', loadChildren: () => import('./agenda/agenda.module').then(m => m.AgendaModule),
+    data: { label: 'Agenda' }
   },
   {
     path: 'catalogue',
-    loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)
+    loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule),
+    data: { label: 'Catalogue' }
   },
   {
     path: 'a-propos',
-    loadChildren: () => import('./a-propos/a-propos.module').then(m => m.AProposModule)
+    loadChildren: () => import('./a-propos/a-propos.module').then(m => m.AProposModule),
+    data: { label: 'A propos' }
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(ROUTES, { relativeLinkResolution: 'legacy', scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(ROUTES, { 
+    relativeLinkResolution: 'legacy',
+    scrollPositionRestoration: 'enabled',
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ROUTE_ANIMATION } from './animations/animations';
 
 @Component({
+  animations: [ROUTE_ANIMATION],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent { }
+export class AppComponent {
+  getRouteAnimation(outlet: RouterOutlet): string {
+    return outlet
+      && outlet.activatedRouteData
+      && outlet.activatedRouteData['label'] as string;
+  }
+}
